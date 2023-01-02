889a5a607038dfa1a445c18f34d700d1becb879e
Sir Darey <darey1oguns@gmail.com>
2023-01-02 11:36:08 +0100

    Days 1 & 2

---
+++ src/_1_290_WordPattern.java
@@ -0,0 +1,39 @@
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @Sir Darey
+ * 
+ * LeetCode Daily Challenge, January 2023
+ * Day 1: Problem 290 - Word Pattern
+ */
+
+public class _1_290_WordPattern {
+    public boolean wordPattern(String pattern, String s) {
+        String [] sArray = s.split(" ");
+        int n = sArray.length;
+
+        if (pattern.length() == n) {
+            Map <Character, String> map = new HashMap<>();
+            Set <String> set = new HashSet<>();
+            for (int i=0; i<n; i++) {
+                char current = pattern.charAt(i);
+                if (map.containsKey(current)) {
+                    if (!map.get(current).equals(sArray[i]))
+                        return false;
+                } else {
+                    if (set.add(sArray[i]))
+                        map.put(current, sArray[i]);
+                    else
+                        return false;
+                }
+            }
+            return true;
+        }
+        return false;       
+    }
+}
---
+++ src/_2_520_DetectCapital.java
@@ -0,0 +1,41 @@
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *
+ * @Sir Darey
+ * 
+ * LeetCode Daily Challenge, January 2023
+ * Day 2: Problem 520 - Detect Capital
+ */
+
+public class _2_520_DetectCapital {
+    public boolean detectCapitalUse(String word) {
+        if (word.length() == 1)
+            return true;
+
+        int firstChar = word.charAt(0);
+        int i = 1;
+
+        if(firstChar - 'a' >= 0 || (firstChar - 'a' < 0 && word.charAt(1) - 'a' >= 0))  {
+            
+            while (i < word.length()) {
+                if(word.charAt(i) - 'a' < 0)
+                    return false;
+                i++;
+            }
+            return true;
+        } else {
+            ++i;
+            while (i < word.length()) {
+                if(word.charAt(i) - 'a' >= 0)
+                    return false;
+                i++;
+            }
+            return true;
+        }
+    }
+}
