2f1ecfd089cc5039f2fd7f87569d916527cb1807
Sir Darey <darey1oguns@gmail.com>
2023-01-09 08:57:23 +0100

    Day  9

---
+++ src/January/_9_144_BinaryTreePreorderTraversal.java
@@ -0,0 +1,42 @@
+package January;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @Sir Darey
+ * 
+ * LeetCode Daily Challenge, January 2023
+ * Day 9: 144 - Binary Tree Preorder Traversal
+ */
+
+public class _9_144_BinaryTreePreorderTraversal {
+    
+    private class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        recur(root, result);
+        return result;
+    }
+
+    void recur(TreeNode root, List<Integer> result) {
+        if (root == null)
+            return;
+        result.add(root.val);
+        recur(root.left, result);
+        recur(root.right, result);
+    }
+}
