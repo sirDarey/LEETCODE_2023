948e3d31baf5bebb3bb190188b3113874f94bc1c
Sir Darey <darey1oguns@gmail.com>
2023-02-01 04:34:23 +0100

    Feb, Day 1

---
+++ src/February/_1_1071_GreatestCommonDivisorOfStrings.java
@@ -0,0 +1,30 @@
+package February;
+
+/**
+*
+* @Sir Darey
+* 
+* LeetCode Problem 1071 - Greatest Common Divisor of Strings
+*/
+
+public class _1_1071_GreatestCommonDivisorOfStrings {
+	
+	public int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        } else {
+            return gcd(y, x % y);
+        }    
+    }
+    
+    public String gcdOfStrings(String str1, String str2) {
+        // Check if they have non-zero GCD string.
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        
+        // Get the GCD of the two lengths.
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+    }
+}
