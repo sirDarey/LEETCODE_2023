59ad1ac9124d7dc046002d820d608a477f3dd67d
Sir Darey <darey1oguns@gmail.com>
2023-02-03 11:37:40 +0100

    Feb, Day 3

---
+++ src/February/_3_6_ZigzagConversion.java
@@ -0,0 +1,31 @@
+package February;
+
+/**
+*
+ @Sir Darey
+ * 
+ * LeetCode Daily Challenge, February 2023
+ * Day 3: 6 - Zigzag Conversion
+*/
+
+public class _3_6_ZigzagConversion {
+	
+	public String convert(String s, int numRows) {
+        if (numRows == 1)
+            return s;
+
+        int jump = 2*(numRows-1);
+        String result = "";
+
+        for (int row=0; row<numRows; row++) {
+
+            for (int i=row; i<s.length(); i+=jump) {
+                result += s.charAt(i);
+
+                if (row > 0 && row < numRows-1 && i+jump-(2*row) < s.length())
+                    result += s.charAt(i+jump-(2*row));
+            }
+        }
+        return result;
+    }
+}
