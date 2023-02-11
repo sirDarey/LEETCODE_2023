5e88d1a6523c3af48a68464345d17919345693df
Sir Darey <darey1oguns@gmail.com>
2023-02-11 20:57:55 +0100

    Problem 788

---
+++ src/February/random/_788_RotatedDigits.java
@@ -0,0 +1,36 @@
+package February.random;
+
+/**
+*
+* @Sir Darey
+* 
+* LeetCode Problem 788 - Rotated Digits
+*/
+
+public class _788_RotatedDigits {
+	
+	public int rotatedDigits(int n) {
+        int result = 0;
+        int [] dp = new int [n+1];
+
+        for (int i=0; i<=n; i++) {
+            if (i < 10) {
+                if (i==0 || i==1 || i==8)
+                    dp[i] = 1;
+                else if (i==2 || i==5 || i==6 || i==9) {
+                    dp[i] = 2;
+                    result++;
+                }
+            } else {
+                int a = dp[i/10], b = dp[i%10];
+                if (a==1 && b==1)
+                    dp[i] = 1;
+                else if (a>=1 && b>=1) {
+                    dp[i] = 2;
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+}
