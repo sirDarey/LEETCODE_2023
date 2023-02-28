5fbcf662590b6741fd1770b79243993a1a05c888
Sir Darey <darey1oguns@gmail.com>
2023-02-28 08:48:56 +0100

    Feb, Day 26

--- src/February/_26_72_EditDistance.java
+++ src/February/_26_72_EditDistance.java
@@ -5,18 +5,33 @@ package February;
 *  @Sir Darey
 * 
 * LeetCode Daily Challenge, February 2023
-* Day 25: Problem 121 - Best Time to Buy and Sell Stock
+* Day 26: Problem 72 - EditDistance
 */
 
 public class _26_72_EditDistance {
 	
-	public int maxProfit(int[] prices) {
-        int result = 0, minCP = prices[0];
+	public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int [][] dp = new int [m+1][n+1];
         
-        for (int i=1; i<prices.length; i++) {
-            result = Math.max(result, prices[i] - minCP);
-            minCP = Math.min(minCP, prices[i]);
+        for (int i=0; i<=m; i++)
+            dp[i][0] = i;
+        for (int j=0; j<=n; j++)
+            dp[0][j] = j;
+        
+        for (int i=1; i<=m; i++) {
+            int x = i-1;
+            
+            for (int j=1; j<=n; j++) {
+                
+                int y = j-1;
+                if (word1.charAt(x) == word2.charAt(y))
+                    dp[i][j] = dp[x][y];
+                else 
+                    dp[i][j] = 1 + Math.min(Math.min(dp[x][y], dp[i][y]), dp[x][j]);
+            }
         }
-        return result;
+        
+        return dp[m][n];
     }
 }
