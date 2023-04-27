5cdcefcd4e98d1690c6263f473d4735f0d1feff1
Sir Darey <darey1oguns@gmail.com>
2023-04-27 09:28:42 +0100

    April, Day 27

---
+++ src/April/_27_319_BulbSwitcher.java
@@ -0,0 +1,16 @@
+package April;
+
+/**  
+* 
+*  @Sir Darey
+* 
+* LeetCode Daily Challenge, April 2023
+* Day 27: Problem 319 - Bulb Switcher
+*/
+
+public class _27_319_BulbSwitcher {
+	
+	public int bulbSwitch(int n) {
+        return (int)Math.sqrt(n);
+    }
+}
