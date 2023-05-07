2d635ce57f90322ef933df260d6afc9d06a67162
Sir Darey <darey1oguns@gmail.com>
2023-05-07 19:32:53 +0100

    Problem 12

--- src/May/Random/_12_IntegerToRoman.java
+++ src/May/Random/_12_IntegerToRoman.java
@@ -1,34 +1,34 @@
 package May.Random;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 /**  
 * 
 *  @Sir Darey
 * 
-* LeetCode Problem 187 - Repeated DNA Sequences
+* LeetCode Problem 12 Integer To Roman
 */
 
 public class _12_IntegerToRoman {
 	
-	public List<String> findRepeatedDnaSequences(String s) {
-        Set<String> set = new HashSet<>();
-        Set<String> result = new HashSet<>();
-        int n = s.length();
-        if (n < 10)
-            return new ArrayList<>(); 
-        
-        int i = 0, j = 10;
-        
-        while (j <= n) {
-            if (!set.add(s.substring(i, j)))
-                result.add(s.substring(i, j));
-            i++; j++;
-        }
+	StringBuilder sb = new StringBuilder();
+    String [] thousands = {"M", "MM", "MMM"};
+    String [] hundreds = {"C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    String [] tens = {"X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    String [] units = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
-        return new ArrayList<>(result);
+    public String intToRoman(int num) {
+        
+        while (num > 0) {
+            if (num >= 1000) num = helper(num, thousands, 1000);
+            else if (num >= 100) num = helper(num, hundreds, 100); 
+            else if(num >= 10) num = helper(num, tens, 10); 
+            else num = helper(num, units, 1);
+        }       
+
+        return sb.toString();
+    }
+
+    int helper (int num, String [] placeValue, int divisor) {
+        sb.append(placeValue[(num/divisor) - 1]);
+        return num%divisor;
     }
 }
