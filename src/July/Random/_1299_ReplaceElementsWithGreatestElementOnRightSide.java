9ad6a3dd09b609658190f064b05be2bf867d5633
Sir Darey <darey1oguns@gmail.com>
2023-07-30 22:44:01 +0100

    Problem 1299

---
+++ src/July/Random/_1299_ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,27 @@
+package July.Random;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**  
+* 
+*  @Sir Darey
+* 
+* LeetCode Problem 1299 - Replace Elements with Greatest Element on Right Side
+* 
+*/
+
+public class _1299_ReplaceElementsWithGreatestElementOnRightSide {
+	
+	public int[] replaceElements(int[] arr) {
+        int n = arr.length, max = -1;
+
+        for (int i=n-1; i>=0; i--) {
+            int temp = arr[i];
+            arr[i] = max;
+            max = Math.max(max, temp);
+        }
+
+        return arr;
+    }
+}
